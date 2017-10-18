32fefab91bde09416136d9164888fb61b8ff0b64
Sebastien Phaneuf <sebastien.phaneuf@autodesk.com>
2017-10-18 07:59:21 -0400

    White spaces cleaning

--- spui/utils.ts
+++ spui/utils.ts
@@ -2,8 +2,8 @@ export interface StringKeyMap<T> {
     [key: string]: T;
 }
 
-export function isFunction(obj): obj is Function { 
-    return typeof obj === 'function'; 
+export function isFunction(obj): obj is Function {
+    return typeof obj === 'function';
 }
 
 export function isNode(obj): obj is Node {
@@ -14,11 +14,11 @@ export function isObject(obj): obj is Object {
     return typeof obj === 'object';
 }
 
-export function isString(obj): obj is string { 
+export function isString(obj): obj is string {
     return typeof obj === 'string'; 
 }
 
-export function expandValue(value): any { 
+export function expandValue(value): any {
     return isFunction(value) ? value() : value; 
 }
 
