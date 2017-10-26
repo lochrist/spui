5699ec1337cf4f07444f8614518fbeb022b954b2
Sebastien Phaneuf <sebastien.phaneuf@autodesk.com>
2017-10-25 21:08:14 -0400

    rename todo-mvc to minotodo + setup todomvc

---
+++ examples/todomvc/index.ts
@@ -0,0 +1,6 @@
+import * as sp from '../../spui/index';
+import * as utils from '../../spui/utils';
+const h = sp.h;
+
+const view = h('div', {}, 'POW!!!')
+document.querySelector('#todoapp').appendChild(view);
